559fd09968c973ab230fbf9442f221bc7d919a29
zhihui.wu@hp.com <mycat1314>
2016-01-08 22:51:29 +0800

    修改单元测试

--- src/test/java/io/mycat/net2/TestMySQLPackageSplitter.java
+++ src/test/java/io/mycat/net2/TestMySQLPackageSplitter.java
@@ -1,5 +1,7 @@
 package io.mycat.net2;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import org.junit.Test;
@@ -9,22 +11,33 @@ import junit.framework.Assert;
 public class TestMySQLPackageSplitter {
 
 	@Test
-	public void testSpliter() {
+	public void testSpliter() throws IOException {
 		String testS = "test data only for ";
 		SharedBufferPool sharedPool = new SharedBufferPool(1024 * 1024 * 100, testS.length() + 4);
 		ReactorBufferPool reactBufferPool = new ReactorBufferPool(sharedPool, Thread.currentThread(), 1000);
 		ByteBufferArray bufArray = reactBufferPool.allocate();
-		//@todo 构造符合MYSQL报文的数据包，进行测试
-		for (int j = 0; j < 10; j++) {
-			ByteBuffer buf = bufArray.addNewBuffer();
-			buf.put(new String(testS + j).getBytes());
-		}
-		int blockCount = bufArray.getBlockCount();
+		bufArray.addNewBuffer();
 		int readBufferOffset = 0;
-		for (int i = 0; i < blockCount; i++) {
-			readBufferOffset = CommonPackageUtil.parsePackages(bufArray, bufArray.getBlock(i), readBufferOffset);
-		}
+		// @todo 构造符合MYSQL报文的数据包，进行测试
 
+		ByteArrayInputStream mysqlPackgsStream = createMysgqlPackages();
+		while (mysqlPackgsStream.available() > 0) {
+			ByteBuffer curBuf = bufArray.getLastByteBuffer();
+			if(!curBuf.hasRemaining())
+			{
+				curBuf=bufArray.addNewBuffer();
+			}
+			byte[] data = new byte[curBuf.remaining()];
+			int readed = mysqlPackgsStream.read(data);
+			curBuf.put(data, 0, readed);
+			readBufferOffset = CommonPackageUtil.parsePackages(bufArray, curBuf, readBufferOffset);
+
+		}
 		Assert.assertEquals(10, bufArray.getCurPacageIndex());
+	}
+
+	private ByteArrayInputStream createMysgqlPackages() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
